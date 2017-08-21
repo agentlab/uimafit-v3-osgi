320cd8d87e34378e9535a125a4eece5b74ceadc8
Alex Ivanov <amivanoff@gmail.com>
2017-08-21 19:13:27 +0300

    Add Example

---
+++ bundles/org.apache.uima.fit.osgi.examples/src/main/java/org/apache/uima/fit/examples/tutorial/ex1/Activator.java
@@ -0,0 +1,101 @@
+package org.apache.uima.fit.examples.tutorial.ex1;
+
+import static org.apache.uima.fit.osgi.utils.AnalysisEngineFactoryOSGi.createEngine;
+import static org.apache.uima.fit.util.JCasUtil.select;
+
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.TypeSystem;
+import org.apache.uima.cas.admin.TypeSystemMgr;
+import org.apache.uima.cas.impl.FSClassRegistry;
+import org.apache.uima.fit.examples.tutorial.type.RoomNumber;
+import org.apache.uima.fit.internal.ResourceManagerFactory;
+import org.apache.uima.fit.osgi.utils.UIMABundleActivator;
+import org.apache.uima.fit.osgi.utils.cl.ClerezzaUIMAExtensionClassLoader;
+import org.apache.uima.fit.osgi.utils.cl.UIMAClassLoaderRepository;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceManager;
+import org.apache.uima.resource.metadata.TypeDescription;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
+@Component
+public class Activator extends UIMABundleActivator {
+	@Reference(policy=ReferencePolicy.STATIC)
+	protected UIMAClassLoaderRepository classLoaderRepository;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	@Activate
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+
+		String text = "The meeting was moved from Yorktown 01-144 to Hawthorne 1S-W33.";
+
+		FSClassRegistry ff;
+
+		ResourceManager resourceManager = ResourceManagerFactory.newResourceManager();
+	    ClassLoader cl = getClass().getClassLoader();
+	    ClassLoader ccl = new ClerezzaUIMAExtensionClassLoader(cl, classLoaderRepository.getComponents());
+	    resourceManager.setExtensionClassPath(ccl, "*", true);
+
+	    //List<TypeSystemDescription> tsdList = new ArrayList<TypeSystemDescription>();
+	    //TypeSystemDescription tsd = mergeTypeSystems(tsdList, resourceManager);
+
+	    AnalysisEngine analysisEngine = createEngine(RoomNumberAnnotator.class, resourceManager);
+
+	    JCas jCas = analysisEngine.newJCas();
+	    jCas.setDocumentText(text);
+
+	    //jCas.
+	    //jCas.getCasImpl().commitTypeSystem();
+
+	    //initTypeSystem(tsd, jCas);
+	    analysisEngine.process(jCas);
+
+	    for (RoomNumber roomNumber : select(jCas, RoomNumber.class)) {
+	      System.out.println(roomNumber.getCoveredText() + "\tbuilding = " + roomNumber.getBuilding());
+	    }
+	}
+
+	protected void classRegistered() {
+		classLoaderRepository.registerComponent(RoomNumberAnnotator.class);
+	}
+
+	/**
+	 * TODO JavaDoc
+	 *
+	 * @param tsd
+	 * @param jCas
+	 */
+	private void initTypeSystem(JCas jCas) {
+		TypeSystemDescription tsd = UIMAFramework.getResourceSpecifierFactory()
+            .createTypeSystemDescription();
+
+		TypeDescription type = tsd.addType("MyType", "", CAS.TYPE_NAME_TOP);
+		TypeSystem ts = jCas.getTypeSystem();
+	    TypeSystemMgr tmr = (TypeSystemMgr)ts;
+	    //tmr.
+
+	    tmr.addType("building", ts.getType(CAS.TYPE_NAME_STRING));
+	    //tmr.add("building", ts.getType(CAS.TYPE_NAME_STRING));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	@Deactivate
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Goodbye World!!");
+	}
+
+}
---
+++ bundles/org.apache.uima.fit.osgi.examples/src/main/java/org/apache/uima/fit/examples/type/Token.java
@@ -0,0 +1,123 @@
+
+
+   
+/* Apache UIMA v3 - First created by JCasGen Fri Jul 21 17:55:45 MSK 2017 */
+
+package org.apache.uima.fit.examples.type;
+
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.impl.TypeSystemImpl;
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+
+
+import org.apache.uima.jcas.tcas.Annotation;
+
+
+/** 
+ * Updated by JCasGen Fri Jul 21 17:55:45 MSK 2017
+ * XML source: C:/git/uimafit-v3-tmp/uimafit-examples/target/jcasgen/typesystem.xml
+ * @generated */
+public class Token extends Annotation {
+ 
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static String _TypeName = "org.apache.uima.fit.examples.type.Token";
+  
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(Token.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated
+   * @return index of the type  
+   */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+ 
+  /* *******************
+   *   Feature Offsets *
+   * *******************/ 
+   
+  public final static String _FeatName_pos = "pos";
+
+
+  /* Feature Adjusted Offsets */
+  public final static int _FI_pos = TypeSystemImpl.getAdjustedFeatureOffset("pos");
+
+   
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected Token() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated
+   * @param casImpl the CAS this Feature Structure belongs to
+   * @param type the type of this Feature Structure 
+   */
+  public Token(TypeImpl type, CASImpl casImpl) {
+    super(type, casImpl);
+    readObject();
+  }
+  
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs 
+   */
+  public Token(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+
+  /** @generated
+   * @param jcas JCas to which this Feature Structure belongs
+   * @param begin offset to the begin spot in the SofA
+   * @param end offset to the end spot in the SofA 
+  */  
+  public Token(JCas jcas, int begin, int end) {
+    super(jcas);
+    setBegin(begin);
+    setEnd(end);
+    readObject();
+  }   
+
+  /** 
+   * <!-- begin-user-doc -->
+   * Write your own initialization here
+   * <!-- end-user-doc -->
+   *
+   * @generated modifiable 
+   */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: pos
+
+  /** getter for pos - gets 
+   * @generated
+   * @return value of the feature 
+   */
+  public String getPos() { return _getStringValueNc(_FI_pos);}
+    
+  /** setter for pos - sets  
+   * @generated
+   * @param v value to set into the feature 
+   */
+  public void setPos(String v) {
+    _setStringValueNfc(_FI_pos, v);
+  }    
+    
+  }
+
+    
